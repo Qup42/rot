01f4531dede405fbb556ce8b9ac868c7a300400b
anais <Mint2017.Rot@UERC115WS03.hpi.uni-potsdam.de>
2017-04-01 13:23:54 +0200

    Der Anfang der Anbindung der ? - Knöpfe grobe Klassenstruktur

---
+++ src/help/Hilfe_Button.java
@@ -0,0 +1,43 @@
+package help;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Hilfe_Button{
+	@FXML
+	Button menu;
+	@FXML
+	Button internet;
+	@FXML
+	Button dokumente;
+	@FXML
+	Button schulbuch;
+	@FXML
+	Button menu;
+	@FXML
+	Button hilfe;
+	@FXML
+	Button menu;
+	@FXML
+	Button hilfe;
+	@FXML
+	Button menu;
+	@FXML
+	Button hilfe;
+
+	@FXML
+	protected void start_hilfe(){
+
+	}
+
+	@FXML
+	protected void dokumente_hilfe(){
+
+	}
+
+	@FXML
+	protected void schulbuch_hilfe(){
+
+	}
+}
