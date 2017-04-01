0b189ac8500c4ac3c37389339bfb677828b680c8
Julian <Mint2017.Rot@UERC115WS01.hpi.uni-potsdam.de>
2017-04-01 13:21:57 +0200

    Update auf neuesten Stand

---
+++ src/application/DokumentWerkzeugSpecification.java
@@ -0,0 +1,15 @@
+package application;
+import java.io.File;
+
+public interface DokumentWerkzeugSpecification {
+
+	public void einVerzeichnisHoch();
+	public void geheInHomeVerzeichnis();
+	public void geheInRootVerzeichnis();
+
+	public void geheInVerzeichnis(File verzeichnis) throws IllegalArgumentException;
+	public void geheInUnterverzeichnis(String verzeichnisName) throws IllegalArgumentException;
+
+
+	public String[] gebeAllesInVerzeichnis();
+}
