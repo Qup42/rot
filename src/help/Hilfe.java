f9b5f51609e77d5e970e3cdf0b18bbc461661e79
mint2017.rot <hpisommercamp2016+mintrot@gmail.com>
2017-04-01 11:31:08 +0000

    Update Hilfe.java Rechtschreibfehler Umlaut Zeile 29 Bildschirmuebertragung behoben

--- src/help/Hilfe.java
+++ src/help/Hilfe.java
@@ -3,39 +3,39 @@ package help;
 
 public class Hilfe {
 
-	public static final String HOME = "Hier können Sie direkt auf den Startbildschirm zurueckkehren.";
-	public static final String ZURUECK = "Hier können Sie eine Seite zurueckgehen.";
+	public static final String HOME = "Hier kÃ¶nnen Sie direkt auf den Startbildschirm zurueckkehren.";
+	public static final String ZURUECK = "Hier kÃ¶nnen Sie eine Seite zurueckgehen.";
 
-	public static final String MENUE = "Hier werden die Menüoptionen angezeigt.";
-	public static final String SUCHE = "Geben Sie das gewünschte Programm oder das Datum der gewünschten Stunde ein. Sie können auch Funktionen suchen.";
-	public static final String STIFT = "Der Stift macht dünne Linien zum Schreiben. Wenn er blau umrandet ist, ist er aktiviert. Zum Deaktivieren bitte auf eine andere Funktion (z.B. Marker...) klicken.";
+	public static final String MENUE = "Hier werden die MenÃ¼optionen angezeigt.";
+	public static final String SUCHE = "Geben Sie das gewÃ¼nschte Programm oder das Datum der gewÃ¼nschten Stunde ein. Sie kÃ¶nnen auch Funktionen suchen.";
+	public static final String STIFT = "Der Stift macht dÃ¼nne Linien zum Schreiben. Wenn er blau umrandet ist, ist er aktiviert. Zum Deaktivieren bitte auf eine andere Funktion (z.B. Marker...) klicken.";
 	public static final String MARKER = "Kann Textpassagen markieren. Wenn er blau umrandet ist, ist er aktiviert. Zum Deaktivieren bitte auf eine andere Funktion (z.B. Marker...) klicken.";
-	public static final String RADIERER = "Kann Markierungen oder auch Linien und Text durch Drüberstreichen entfernen. Wenn er blau umrandet ist, ist er aktiviert. Zum Deaktivieren bitte auf eine andere Funktion (z.B. Marker...) klicken.";
-	public static final String FARBE = "Hier kann die Schrift- bzw. Markerfarbe geändert werden, wenn der Stift bzw. Marker ausgewählt ist.";
-	public static final String HINTERGRUND = "Hier können Sie den Hintergrund ändern: liniert, kariert oder weiß";
-	public static final String GEOMETRIE = "Hier können Sie Geometriefunktionen nutzen. Sie können Graphen zeichnen und mit Funktionen arbeiten.";
+	public static final String RADIERER = "Kann Markierungen oder auch Linien und Text durch DrÃ¼berstreichen entfernen. Wenn er blau umrandet ist, ist er aktiviert. Zum Deaktivieren bitte auf eine andere Funktion (z.B. Marker...) klicken.";
+	public static final String FARBE = "Hier kann die Schrift- bzw. Markerfarbe geÃ¤ndert werden, wenn der Stift bzw. Marker ausgewÃ¤hlt ist.";
+	public static final String HINTERGRUND = "Hier kÃ¶nnen Sie den Hintergrund Ã¤ndern: liniert, kariert oder weiÃŸ";
+	public static final String GEOMETRIE = "Hier kÃ¶nnen Sie Geometriefunktionen nutzen. Sie kÃ¶nnen Graphen zeichnen und mit Funktionen arbeiten.";
 	public static final String HILFE = "Hier werden Informationen zur aktuellen Seite angezeigt. Die Hilfe wird durch Anklicken aktiviert und durch erneutes Anklicken deaktiviert.";
-	public static final String CLOUD_UPLOAD = "Hier können Sie ihr aktuelles Tafelbild in die Cloud stellen, sodass es von Zuhause angeschaut werden kann.";
-	public static final String SEITEN = "Hier können Sie ihre benutzen Seiten anschauen und durch Klicken eine Seite auswählen. Unten kann eine neue Seite hinzugefuegt werden.";
-	public static final String SEITEN_PLUS = "Hier können Sie eine neue Seite hinzufügen. Dies resultiert in einer weißen Tafel. Über dieser Funktion finden sich dann die alten Seiten.";
-	public static final String SEITEN_VORHER = "Hier können Sie hochscrollen und vorherige Seiten anschauen.";
-	public static final String SEITEN_NACHHER = "Hier können Sie herunterscrollen und neuere Seiten anschauen.";
+	public static final String CLOUD_UPLOAD = "Hier kÃ¶nnen Sie ihr aktuelles Tafelbild in die Cloud stellen, sodass es von Zuhause angeschaut werden kann.";
+	public static final String SEITEN = "Hier kÃ¶nnen Sie ihre benutzen Seiten anschauen und durch Klicken eine Seite auswÃ¤hlen. Unten kann eine neue Seite hinzugefuegt werden.";
+	public static final String SEITEN_PLUS = "Hier kÃ¶nnen Sie eine neue Seite hinzufÃ¼gen. Dies resultiert in einer weiÃŸen Tafel. Ãœber dieser Funktion finden sich dann die alten Seiten.";
+	public static final String SEITEN_VORHER = "Hier kÃ¶nnen Sie hochscrollen und vorherige Seiten anschauen.";
+	public static final String SEITEN_NACHHER = "Hier kÃ¶nnen Sie herunterscrollen und neuere Seiten anschauen.";
 
-	public static final String INTERNET = "Hier können Sie ins Internet gehen.";
-	public static final String DOKUMENTE = "Hier können Sie die Tafelbilder vergangener Stunden anschauen.";
-	public static final String SCHULBUCH = "Hier können Sie die Schulbücher Ihres aktuellen Kurses einsehen.";
-	public static final String KLASSE_WECHSELN = "Haben Sie sich für den falschen Kurs angemeldet? Dann können Sie hier ihren Kurs ändern.";
-	public static final String PROGRAMME = "Hier können Sie fachspeziefische Programme herunterladen und nutzen, um ihren Unterricht abwechslungsreicher zu gestalten.";
-	public static final String BILDSCHIRMÜBERTRAGUNG = "Hier können Sie einen anderen Bildschirm auf dieser Tafel anzeigen lassen.";
-	public static final String ABMELDEN = "Hier können Sie sich abmelden. Ihr aktuelles Tafelbild wird automatisch mit dem Datum der Stunde unter Dokumente gespeichert.";
+	public static final String INTERNET = "Hier kÃ¶nnen Sie ins Internet gehen.";
+	public static final String DOKUMENTE = "Hier kÃ¶nnen Sie die Tafelbilder vergangener Stunden anschauen.";
+	public static final String SCHULBUCH = "Hier kÃ¶nnen Sie die SchulbÃ¼cher Ihres aktuellen Kurses einsehen.";
+	public static final String KLASSE_WECHSELN = "Haben Sie sich fÃ¼r den falschen Kurs angemeldet? Dann kÃ¶nnen Sie hier ihren Kurs Ã¤ndern.";
+	public static final String PROGRAMME = "Hier kÃ¶nnen Sie fachspeziefische Programme herunterladen und nutzen, um ihren Unterricht abwechslungsreicher zu gestalten.";
+	public static final String BILDSCHIRMUEBERTRAGUNG = "Hier kÃ¶nnen Sie einen anderen Bildschirm auf dieser Tafel anzeigen lassen.";
+	public static final String ABMELDEN = "Hier kÃ¶nnen Sie sich abmelden. Ihr aktuelles Tafelbild wird automatisch mit dem Datum der Stunde unter Dokumente gespeichert.";
 
-	public static final String AUSWAHL_DOKUMENTE = "Hier können Sie durch Klicken das Dokument auswählen. Es wird dann automatisch geöffnet.";
+	public static final String AUSWAHL_DOKUMENTE = "Hier kÃ¶nnen Sie durch Klicken das Dokument auswÃ¤hlen. Es wird dann automatisch geÃ¶ffnet.";
 
-	public static final String BUCH_AUSWAHL = "Hier können Sie durch Klicken das gewünschte Buch auswählen. Es wird dann automatisch geöffnet.";
-	public static final String BUCH_VOR = "Hier können Sie eine Seite weiter aufschlagen.";
-	public static final String BUCH_ZURUECK = "Hier können Sie eine Seite zurück gehen.";
-	public static final String BUCH_AENDERN = "Hier können Sie die gewünschte Seitenzahl eingeben.";
-	public static final String BUCH_SUCHE = "Hier können Sie ein Schlagwort/eine Kapitelüberschrift suchen.";
-	public static final String BUCH_INHALTSVERZEICHNIS = "Hier können Sie das Inhaltsverzeichnis des Buches Abrufen.";
+	public static final String BUCH_AUSWAHL = "Hier kÃ¶nnen Sie durch Klicken das gewÃ¼nschte Buch auswÃ¤hlen. Es wird dann automatisch geÃ¶ffnet.";
+	public static final String BUCH_VOR = "Hier kÃ¶nnen Sie eine Seite weiter aufschlagen.";
+	public static final String BUCH_ZURUECK = "Hier kÃ¶nnen Sie eine Seite zurÃ¼ck gehen.";
+	public static final String BUCH_AENDERN = "Hier kÃ¶nnen Sie die gewÃ¼nschte Seitenzahl eingeben.";
+	public static final String BUCH_SUCHE = "Hier kÃ¶nnen Sie ein Schlagwort/eine KapitelÃ¼berschrift suchen.";
+	public static final String BUCH_INHALTSVERZEICHNIS = "Hier kÃ¶nnen Sie das Inhaltsverzeichnis des Buches Abrufen.";
 
 }
