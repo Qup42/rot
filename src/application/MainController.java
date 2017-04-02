9225398c1f1a84c010c2aff189c5fe0f628f1f06
Josy <Mint2017.Rot@UERC115WS04.hpi.uni-potsdam.de>
2017-04-02 16:47:23 +0200

    Farbige Unterlegung bei AUswahl implementiert

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -19,18 +19,26 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class MainController implements Initializable{
+public class MainController implements Initializable {
 
 	Seiten aktuelleSeite = Seiten.STARTSEITE;
 
 	@FXML
 	public Canvas canvas;
+	@FXML
+	public ImageView pencilb;
+	public ImageView markerb;
+	public ImageView rubberb;
+	public ImageView backgroundb;
+	public ImageView farbeb;
+	
 
 	public GraphicsContext graphicsContext;
 	public Color color = Color.DARKBLUE;
@@ -40,90 +48,86 @@ public class MainController implements Initializable{
 
 	}
 
-
-
 	public enum Tool {
-		Stift,Marker,Radierer
+		Stift, Marker, Radierer
 	}
 
-
-	public void makeDrawable(Tool tool)
-	{
+	public void makeDrawable(Tool tool) {
 		graphicsContext = canvas.getGraphicsContext2D();
-		canvas.addEventHandler(MouseEvent.MOUSE_PRESSED,
-	                new EventHandler<MouseEvent>(){
+		canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
 
-	            @Override
-	            public void handle(MouseEvent event) {
-	                graphicsContext.beginPath();
-	                graphicsContext.moveTo(event.getX(), event.getY());
-	                setParameters(tool);
-	            }
-	        });
+			@Override
+			public void handle(MouseEvent event) {
+				graphicsContext.beginPath();
+				graphicsContext.moveTo(event.getX(), event.getY());
+				setParameters(tool);
+			}
+		});
 
-	        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
-	                new EventHandler<MouseEvent>(){
+		canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
 
-	            @Override
-	            public void handle(MouseEvent event) {
-	                graphicsContext.lineTo(event.getX(), event.getY());
-	                graphicsContext.stroke();
-	                setParameters(tool);
-	            }
-	        });
-	            
+			@Override
+			public void handle(MouseEvent event) {
+				graphicsContext.lineTo(event.getX(), event.getY());
+				graphicsContext.stroke();
+				setParameters(tool);
+			}
+		});
+
 	}
 
-	private void setParameters(Tool tool)
-	{
+	private void setParameters(Tool tool) {
 		double alpha = 1;
 		double lineWidth = 1;
 		Color tempStroke = this.color;
 		Color tempFill = this.color;
-		switch(tool)
-		{
+		switch (tool) {
 		case Stift:
-			alpha = 1; lineWidth = 1;
+			alpha = 1;
+			lineWidth = 1;
 			break;
 		case Marker:
-			alpha = 0.1; lineWidth = 10; 
+			alpha = 0.1;
+			lineWidth = 10;
 			break;
 		case Radierer:
-			alpha = 1; lineWidth = 5; tempStroke = Color.WHITE; tempFill = Color.WHITE;
+			alpha = 1;
+			lineWidth = 5;
+			tempStroke = Color.WHITE;
+			tempFill = Color.WHITE;
 			break;
 		}
 		graphicsContext.setGlobalAlpha(alpha);
-        graphicsContext.setLineWidth(lineWidth);
-        graphicsContext.setStroke(tempStroke);
-        graphicsContext.setFill(tempFill);
-        graphicsContext.stroke();
+		graphicsContext.setLineWidth(lineWidth);
+		graphicsContext.setStroke(tempStroke);
+		graphicsContext.setFill(tempFill);
+		graphicsContext.stroke();
 	}
-	
-	public void save(Stage primaryStage){
+
+	public void save(Stage primaryStage) {
 		FileChooser fileChooser = new FileChooser();
 
-        //Set extension filter
-        FileChooser.ExtensionFilter extFilter =
-                new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
-        fileChooser.getExtensionFilters().add(extFilter);
+		// Set extension filter
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+		fileChooser.getExtensionFilters().add(extFilter);
 
-        //Show save file dialog
-        File file = fileChooser.showSaveDialog(primaryStage);
+		// Show save file dialog
+		File file = fileChooser.showSaveDialog(primaryStage);
 
-        if(file != null){
-            try {
-                WritableImage writableImage = new WritableImage((int)canvas.getWidth(), (int)canvas.getHeight());
-                canvas.snapshot(null, writableImage);
-                RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
-                ImageIO.write(renderedImage, "png", file);
-            } catch (IOException ex) {
+		if (file != null) {
+			try {
+				WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+				canvas.snapshot(null, writableImage);
+				RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+				ImageIO.write(renderedImage, "png", file);
+			} catch (IOException ex) {
 
-            }
-        }
+			}
+		}
 	}
 
-	public void hilfe(){
-		switch(aktuelleSeite){
+	public void hilfe() {
+		switch (aktuelleSeite) {
 		case STARTSEITE:
 			HilfeController.startHilfe();
 			break;
@@ -133,22 +137,50 @@ public class MainController implements Initializable{
 		case SCHULBUCHSEITE:
 			HilfeController.schulbuchHilfe();
 			break;
-			}
+		}
 	}
-	
-	
-	
-	
-	public void onPenClick()
-	{
+
+	@FXML
+	ImageView stift;
+
+	public void onPenClick(){
+		rubberb.setVisible(false);
+		backgroundb.setVisible(false);
+		farbeb.setVisible(false);
+		markerb.setVisible(false);
 		makeDrawable(MainController.Tool.Stift);
-	}
-	public void onMarkerClick()
-	{
+		pencilb.setVisible(true);
+		/*stift.setOnMouseClicked(new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent e){
+				makeDrawable(MainController.Tool.Stift);
+				pencilb.setVisible(true);
+			}
+			//makeDrawable(MainController.Tool.Stift);
+
+
+
+	});pencilb.setVisible(true);
+	*/	}
+
+
+
+	public void onMarkerClick() {
+		pencilb.setVisible(false);
+		rubberb.setVisible(false);
+		backgroundb.setVisible(false);
+		farbeb.setVisible(false);
+		markerb.setVisible(true);
 		makeDrawable(MainController.Tool.Marker);
 	}
-	public void onEraserClick()
-	{
+
+	public void onEraserClick() {
+		pencilb.setVisible(false);
+		rubberb.setVisible(true);
+		backgroundb.setVisible(false);
+		farbeb.setVisible(false);
+		markerb.setVisible(false);
+		
 		makeDrawable(MainController.Tool.Radierer);
 	}
 	
