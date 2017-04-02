533964879af44c037fc77ec04008f39c1b610ff1
Hartmut <Mint2017.Rot@UERC115WS02.hpi.uni-potsdam.de>
2017-04-02 15:11:52 +0200

    Neue Controller in Main Klasse

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,25 +5,61 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import login.LoginCallback;
+import login.LoginController;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 
-public class Main extends Application {
+public class Main extends Application implements LoginCallback{
 
 	 private Stage primaryStage;
-	 private MainController controller;
 
 	    @Override
 		public void start(Stage primaryStage) {
 	        this.primaryStage = primaryStage;
 	        this.primaryStage.setTitle("SpeedBoard");
-	        try {
+
+	        showLogin();
+
+		}
+
+		public static void main(String[] args) {
+			launch(args);
+		}
+
+		@Override
+		public void login(String klasse, String fach, String lehrkörper) {
+			showMain();
+
+		}
+
+		private void showLogin()
+		{
+			try {
+	            // Load layout from fxml file.
+	        	FXMLLoader loader = new FXMLLoader(getClass().getResource("../login/Login.fxml"));
+
+	            AnchorPane layout  = (AnchorPane) loader.load();
+	        	LoginController controller = loader.getController();
+	        	controller.setData();
+	        	controller.setCallback(this);
+	            // Show the scene containing the root layout.
+	            Scene scene = new Scene(layout);
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+		private void showMain()
+		{
+			try {
 	            // Load layout from fxml file.
 	        	FXMLLoader loader = new FXMLLoader(getClass().getResource("Start.fxml"));
 
 	            AnchorPane layout  = (AnchorPane) loader.load();
-	        	controller = loader.getController();
+	        	MainController controller = loader.getController();
 	        	controller.makeDrawable();
 	            // Show the scene containing the root layout.
 	            Scene scene = new Scene(layout);
@@ -34,8 +70,4 @@ public class Main extends Application {
 	        }
 		}
 
-		public static void main(String[] args) {
-			launch(args);
-		}
-		
 }
