5fd0963235ad596452f85598f5e91b1547dfc4a8
Julian <Mint2017.Rot@UERC115WS01.hpi.uni-potsdam.de>
2017-04-02 12:01:55 +0200

    Kleinere Änderungen an der Dokumente Seite

--- src/dokumente/DokumenteWerkzeug.java
+++ src/dokumente/DokumenteWerkzeug.java
@@ -1,5 +1,12 @@
 package dokumente;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 
 public class DokumenteWerkzeug implements DokumentWerkzeugSpecification{
@@ -39,6 +46,11 @@ public class DokumenteWerkzeug implements DokumentWerkzeugSpecification{
 	}
 
 	@Override
+	public File[] gebeAlleDateienInVerzeichnis() {
+		return aktuellesVerzeichnis.listFiles();
+	}
+
+	@Override
 	public void geheInVerzeichnis(File verzeichnis) throws IllegalArgumentException {
 		if(verzeichnis.isDirectory())
 		{
@@ -63,9 +75,58 @@ public class DokumenteWerkzeug implements DokumentWerkzeugSpecification{
 		}
 	}
 
-	@Override
-	public File[] gebeAlleDateienInVerzeichnis() {
-		return aktuellesVerzeichnis.listFiles();
-	}
+	public static class Finder
+    extends SimpleFileVisitor<Path> {
+
+    private final PathMatcher matcher;
+    private int numMatches = 0;
+
+    Finder(String pattern) {
+        matcher = FileSystems.getDefault()
+                .getPathMatcher("glob:" + pattern);
+    }
+
+    // Compares the glob pattern against
+    // the file or directory name.
+    void find(Path file) {
+        Path name = file.getFileName();
+        if (name != null && matcher.matches(name)) {
+            numMatches++;
+            System.out.println(file);
+        }
+    }
+
+    // Prints the total number of
+    // matches to standard out.
+    void done() {
+        System.out.println("Matched: "
+            + numMatches);
+    }
+
+    // Invoke the pattern matching
+    // method on each file.
+    @Override
+    public FileVisitResult visitFile(Path file,
+            BasicFileAttributes attrs) {
+        find(file);
+        return FileVisitResult.CONTINUE;
+    }
+
+    // Invoke the pattern matching
+    // method on each directory.
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir,
+            BasicFileAttributes attrs) {
+        find(dir);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file,
+            IOException exc) {
+        System.err.println(exc);
+        return FileVisitResult.CONTINUE;
+    }
+}
 
 }
